7e734407a60344c14bad9fbc657789d12d6b93f1
ClickerOfThings <Prikolist>
2022-10-19 19:00:33 +0400

    Add project files.

---
+++ Model/PDFFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using iText.Kernel.Pdf;
+
+namespace TemplateFromPDF.Model
+{
+    internal class PDFFile
+    {
+        
+    }
+}
