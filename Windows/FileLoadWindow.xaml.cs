711d4e7ef599af545aa8c5dbf93f7ebe4a0b7b37
ClickerOfThings <Prikolist>
2022-10-19 22:45:46 +0400

    feat!: логика и дизайн окон приложения - загрузка файлов и просмотр полей

--- Windows/FileLoadWindow.xaml.cs
+++ Windows/FileLoadWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using TemplateFromPDF.Model;
+
 namespace TemplateFromPDF.Windows
 {
     /// <summary>
@@ -20,9 +24,61 @@ namespace TemplateFromPDF.Windows
     /// </summary>
     public partial class FileLoadWindow : Window
     {
+        public ObservableCollection<string> pdfFilesPaths { get; set; }
+            = new ObservableCollection<string>();
+
         public FileLoadWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+        }
+
+        // TODO организовать извлечение путей файлов в отдельный файл/класс
+        private void LoadPDFFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openPDFDialog = new OpenFileDialog();
+            openPDFDialog.Filter = "PDF файлы (.pdf)|*.pdf";
+            openPDFDialog.Multiselect = true;
+
+            bool? result = openPDFDialog.ShowDialog();
+
+            if (result is true)
+            {
+                string[] selectedFiles = openPDFDialog.FileNames;
+                if (selectedFiles.Length == 0)
+                    return;
+
+                foreach(string selectedFile in selectedFiles)
+                    if (!pdfFilesPaths.Contains(selectedFile))
+                        pdfFilesPaths.Add(selectedFile);
+            }
+        }
+
+        private void DeletePDFFilesFromListButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedPDFFiles = new List<string>(PDFFilesListBox.SelectedItems.Cast<string>());
+            foreach (string selectedFile in selectedPDFFiles)
+                pdfFilesPaths.Remove(selectedFile);
+        }
+
+        private void ProcessPDFFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (pdfFilesPaths.Count == 0)
+            {
+                MessageBox.Show("Не выбраны PDF файлы для обработки. Выберите хотя бы 1 файл", "Не выбраны файлы",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            PDFFieldsWindow fieldsWindow = new PDFFieldsWindow(pdfFilesPaths.ToArray());
+
+            this.Hide();
+            fieldsWindow.ShowDialog();
+            this.Show();
         }
     }
 }
---
+++ Windows/PDFFieldsWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using iText.Layout.Element;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using TemplateFromPDF.Model;
+
+namespace TemplateFromPDF.Windows
+{
+    /// <summary>
+    /// Interaction logic for PDFFieldsWindow.xaml
+    /// </summary>
+    public partial class PDFFieldsWindow : Window
+    {
+        private readonly string[] pdfFilesPaths;
+        private readonly List<Dictionary<string, string>> fieldsList = new List<Dictionary<string, string>>();
+
+        public PDFFieldsWindow(string[] pdfFilesPaths)
+        {
+            InitializeComponent();
+
+            this.pdfFilesPaths = pdfFilesPaths;
+        }
+        
+        // TODO организовать создание словарей и колонок в отдельный файл/класс
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(string pdfFilePath in pdfFilesPaths)
+            {
+                PDFFile currentPDFFile = PDFFile.LoadFromFile(pdfFilePath);
+
+                fieldsList.Add(currentPDFFile.Fields);
+            }
+
+            var columns = new List<DataGridTextColumn>();
+            foreach (Dictionary<string, string> dict in fieldsList)
+            {
+                foreach(string key in dict.Keys)
+                {
+                    if (columns.Any(x => (string)x.Header == key))
+                        continue;
+                    DataGridTextColumn column = new DataGridTextColumn();
+                    column.Header = key;
+                    column.Binding = new Binding($"[{key}]");
+                    columns.Add(column);
+                }
+            }
+            foreach(DataGridTextColumn col in columns)
+            {
+                PDFFilesFieldsDataGrid.Columns.Add(col);
+            }
+
+            PDFFilesFieldsDataGrid.ItemsSource = fieldsList;
+        }
+
+        private void BackToFileLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show
+                ("Все изменения в полях будут утеряны, точно вернуться к выбору файлов?", "Назад",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) 
+                == MessageBoxResult.No)
+                return;
+            this.Close();
+        }
+
+        private void SaveNewPDFsToFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog folderChooseDialog = new CommonOpenFileDialog();
+            folderChooseDialog.IsFolderPicker = true;
+            folderChooseDialog.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+
+            if (folderChooseDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                // TODO вставить сохранение в шаблон
+                if (MessageBox.Show($"Файлы сохранены в папку {folderChooseDialog.FileName}. Продолжить редактирование полей?",
+                    "Успешно сохранено", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+                    this.Close();
+            }
+        }
+    }
+}
